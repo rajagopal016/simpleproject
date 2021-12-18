574800b05f60081b2256f917bf8e044552b8a34b
rajagopal016 <33349229+rajagopal016@users.noreply.github.com>
2021-12-18 16:09:40 +0530

    Update MyController.java

--- src/main/java/com/rajagopal/controller/MyController.java
+++ src/main/java/com/rajagopal/controller/MyController.java
@@ -12,7 +12,7 @@ public class MyController {
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
 		LocalDateTime now = LocalDateTime.now();  
 		System.out.println(dtf.format(now));  
-		return "time Now in US EAST is " + dtf.format(now);  
+		return "Time Now in US EAST is " + dtf.format(now);  
 	}  
 
 }
