76e89fcdc865e74aa2cf8edec09886a9fb87f8d9
rajagopal016 <33349229+rajagopal016@users.noreply.github.com>
2021-12-18 21:15:38 +0530

    Update MyController.java

--- src/main/java/com/rajagopal/controller/MyController.java
+++ src/main/java/com/rajagopal/controller/MyController.java
@@ -14,5 +14,14 @@ public class MyController {
 		System.out.println(dtf.format(now));  
 		return "Time now in US EAST is " + dtf.format(now);  
 	}  
+	@RequestMapping("/welcome")  
+	public String welcome()   
+	{  
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+		LocalDateTime now = LocalDateTime.now();  
+		System.out.println(dtf.format(now));  
+		return "Time now in US EAST is " + dtf.format(now);  
+	}  
+
 
 }
