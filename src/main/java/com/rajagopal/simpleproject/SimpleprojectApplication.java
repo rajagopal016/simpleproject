3ead0f2e149acf4b8f00e29f2f153b3888ce2aa5
rajagopal <rajagopal016@gmail.com>
2021-12-19 18:11:35 +0530

    added extends

--- src/main/java/com/rajagopal/simpleproject/SimpleprojectApplication.java
+++ src/main/java/com/rajagopal/simpleproject/SimpleprojectApplication.java
@@ -2,11 +2,12 @@ package com.rajagopal.simpleproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages={"com.rajagopal"})
-public class SimpleprojectApplication {
+public class SimpleprojectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleprojectApplication.class, args);
